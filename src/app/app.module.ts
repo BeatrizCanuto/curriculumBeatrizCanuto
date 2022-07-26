8c9fa235c72cf41985b1112bfa804a067ca80c1c
BeatrizCanuto <BeatrizCanuto>
2022-07-25 22:27:33 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { OrderModule } from 'ngx-order-pipe';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BiografiaComponent } from './biografia/biografia.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { CompetenciasComponent } from './competencias/competencias.component';
+import { ContatoComponent } from './contato/contato.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    InicioComponent,
+    BiografiaComponent,
+    ProjetosComponent,
+    TecnologiasComponent,
+    CompetenciasComponent,
+    ContatoComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ModalModule.forRoot(),
+    OrderModule
+  ],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
